6178c888947934b46fa200afc37b2611e499b217
namdang0219 <namdang.contact@gmail.com>
2025-11-07 11:14:22 +0900

    add: topbar ui

--- app/page.tsx
+++ app/page.tsx
@@ -1,65 +1,73 @@
+import React from "react";
+import styles from "./homeStyle.module.css";
 import Image from "next/image";
 
-export default function Home() {
-  return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
-    </div>
-  );
-}
+import logo from "./../public/logo.png";
+import heartIcon from "./../public/heart.svg";
+import cartIcon from "./../public/buy.svg";
+import profileIcon from "./../public/profile.svg";
+import searchIcon from "./../public/searchIcon.svg";
+
+const HomePage = () => {
+	return (
+		<div>
+			{/* TOP BAR */}
+			<div className={styles.topBar}>
+				<div className={styles.topBarContainer}>
+					{/* LOGO */}
+					<div className={styles.logoContainer}>
+						<Image src={logo} alt="image" />
+
+						<h1 className={styles.logoText}>Comforty</h1>
+					</div>
+
+					{/* SEARCH BAR */}
+					<div className={styles.searchContainer}>
+						<input
+							type="text"
+							placeholder="Search here..."
+							className={styles.searchInput}
+						/>
+
+            <Image src={searchIcon} alt="search-icon" width={22} height={22} />
+					</div>
+
+					{/* FEATURE */}
+					<div className={styles.featureContainer}>
+						{/* Cart Btn */}
+						<div className={styles.featureBtn}>
+							<div className={styles.cartBtnLeft}>
+								<Image src={cartIcon} alt="cart-icon" />
+
+								<span>Cart</span>
+							</div>
+
+							<div className={styles.cartBtnQuantity}>1</div>
+						</div>
+
+						{/* Favorite */}
+						<div
+							className={`${styles.featureBtn} ${styles.squareBtn}`}
+						>
+							<Image
+								src={heartIcon}
+								alt="heart-icon"
+								width={22}
+								height={22}
+							/>
+						</div>
+
+						{/* User */}
+						<div
+							className={`${styles.featureBtn} ${styles.squareBtn}`}
+						>
+							<Image src={profileIcon} alt="user-icon" />
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default HomePage;
